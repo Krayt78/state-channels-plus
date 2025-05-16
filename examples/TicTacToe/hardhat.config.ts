4be8c2548d2f2846a8b99a3098eb1ff249a18d61
krayt78 <ludovic.domingues96@gmail.com>
2025-05-16 12:38:52 +0200

    fixed alias issue

--- examples/TicTacToe/hardhat.config.ts
+++ examples/TicTacToe/hardhat.config.ts
@@ -1,3 +1,24 @@
+import "tsconfig-paths/register";
+import path from "path";
+import moduleAlias from "module-alias";
+
+// Resolve the path to the dependency's "src" directory within its "dist" folder.
+// This is where modules like 'Clock.js' are expected to be if '@/' is used as a prefix
+// by the compiled code in '@peer3/state-channels-plus/dist/src/index.js'.
+const dependencyInternalSrcPath = path.resolve(
+    __dirname, // Current directory of hardhat.config.ts (examples/TicTacToe)
+    "node_modules",
+    "@peer3",
+    "state-channels-plus",
+    "dist",
+    "src"
+);
+
+// Add the alias for the dependency.
+// When require('@/Clock') is encountered from the dependency,
+// it will look for 'Clock.js' (or Clock/index.js etc.) in dependencyInternalSrcPath.
+moduleAlias.addAlias("@", dependencyInternalSrcPath);
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
